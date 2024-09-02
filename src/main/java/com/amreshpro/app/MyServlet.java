5ae6a50c6746fe620e771d959d09f0bc5b378278
Amresh Maurya <amresh.terminal@gmail.com>
2024-09-02 10:42:36 +0530

    servlet form

---
+++ src/main/java/com/amreshpro/app/MyServlet.java
@@ -0,0 +1,22 @@
+package com.amreshpro.app;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/submit")
+public class MyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        String name = request.getParameter("username");
+        PrintWriter out = response.getWriter();
+        out.println("Hello " + name + "!");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="./submit" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username">
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
